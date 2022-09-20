df8b074ad0f22e30ba5d5aad353dbd272f85b11a
mirzi94 <114011541+mirzi94@users.noreply.github.com>
2022-09-20 19:33:36 +0300

    first commit

---
+++ UnityGame/Assets/Scripts/movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Camera cam;
+    Collider planecollider;
+    RaycastHit hit;
+    Ray ray;
+
+    void Start()
+
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        planecollider = GameObject.Find("Plane").GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider == planecollider)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * 4);
+                transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+            }
+        }
+    }
+}
